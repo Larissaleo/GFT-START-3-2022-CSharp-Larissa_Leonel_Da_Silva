8b7aa911409df69da24cab39dd93bee319c8946a
Larissa Leonel da Silva <56083152+Larissaleo@users.noreply.github.com>
2022-02-02 17:20:51 -0300

    Add files via upload

---
+++ FraseInvertida/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace FraseInvertida
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            bool validacao;
+
+            frase = Console.ReadLine();
+            System.Console.WriteLine("Por favor insira o texto/frase a ser invertido");
+            string Inverterfrase = new string(frase.Reverse().ToArray());
+
+            if(frase == Inverterfrase)
+            {
+                validacao = true;
+            }
+            else{
+                validacao = false;
+            }
+
+            System.Console.WriteLine($"Texto/Frase invertido(a): {Inverterfrase} --> {validacao}");
+        }
+            
+
+            
+    }
+}
+
